1a18857bb4bc3d5486c5b1047c2433f961160e00
GERSONS_mapfre <gersons@mapfre.net>
2026-01-30 10:06:17 -0400

    Se completo el laboratorio numero 3

--- Ejecicios_CloudFormation/laboratorio3/source/02-02-desplegar-lambda-test-laboratorio-3.sh
+++ Ejecicios_CloudFormation/laboratorio3/source/02-02-desplegar-lambda-test-laboratorio-3.sh
@@ -1,11 +1,11 @@
 #   Crear Stack
-aws cloudformation create-stack --stack-name LambdaTestLaboratorio3Stack --template-body file://02-01-lambda-test-laboratorio-3.json --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name lambda-laboratorio-3-stack --template-body file://02-01-lambda-test-laboratorio-3.json --capabilities CAPABILITY_NAMED_IAM
 
 #   Actualizarlar Stack
-aws cloudformation update-stack --stack-name LambdaTestLaboratorio3Stack --template-body file://02-01-lambda-test-laboratorio-3.json --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation update-stack --stack-name lambda-laboratorio-3-stack --template-body file://02-01-lambda-test-laboratorio-3.json --capabilities CAPABILITY_NAMED_IAM
 
 #   Eliminar Stack
-aws cloudformation delete-stack --stack-name LambdaTestLaboratorio3Stack
+aws cloudformation delete-stack --stack-name lambda-laboratorio-3-stack
 
 #   Deploy
-aws cloudformation deploy --template-file 02-01-lambda-test-laboratorio-3.json --stack-name LambdaTestLaboratorio3Stack --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file 02-01-lambda-test-laboratorio-3.json --stack-name lambda-laboratorio-3-stack --capabilities CAPABILITY_NAMED_IAM
